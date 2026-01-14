7cee7e0bda9ee90e9f91e02b34c1ec4b85c5022c
Shraddha <nshraddha363@gmail.com>
2026-01-14 20:52:04 +0530

    Initial Express frontend

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Express frontend is running");
+});
+
+app.listen(3000, "0.0.0.0", () => {
+  console.log("Server running on port 3000");
+});
